132d277de01bb25e7af25d43e79fa46fb523fc53
Jakob Lover <jakob.lover@ebnett.no>
2017-05-19 21:49:17 +0200

    doesnt work

---
+++ Code/Imponator/SPI.c
@@ -0,0 +1,35 @@
+/*
+ * SPI.c
+ *
+ * Created: 18-May-17 8:28:57 PM
+ *  Author: Jakob
+ */ 
+
+#include <avr/io.h>
+#include <avr/xmega.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include "SPI.h"
+
+void spi_init(){
+	PORTC.DIRSET |= 1 << 0; //Latch pin for TLC5947
+	PORTC.DIRSET |= 1 << 1; //BLANK pin for TLC5947
+	PORTC.DIRSET |= 1 << 7; //SCK pin for TLC5947
+	PORTC.DIRSET |= 1 << 5; //MOSI pin for TLC5947
+	PORTC.DIRSET |= 1 << 4; //Slave select
+	//PORTC.DIRSET |= 0b10110011;
+	
+	PORTC.PIN4CTRL |= PORT_OPC_PULLUP_gc; //Slave select requires a pull-up i guess
+	PORTC.OUTSET |= 1 << 4; //Slave select HIGH. This is to prevent the XMEGA from being reset to slave
+	PORTC.OUTCLR |= 1 << 0; //Set latch pin low
+	
+	SPIC.CTRL |= SPI_ENABLE_bm | SPI_MASTER_bm | SPI_MODE_0_gc; //Enable SPI, select as master, Mode 0 SPI
+	SPIC.INTCTRL |= SPI_INTLVL_OFF_gc; //turn off interrupts. for now.
+	
+}
+
+void SPI_blankAndLatch(void){
+		PORTC.DIRSET |= 1 << 0;
+		PORTC.DIRCLR |= 1 << 0;
+}
+	
--- Code/Imponator/main.c
+++ Code/Imponator/main.c
@@ -5,11 +5,7 @@
  * Author : Jakob
  */ 
 
-#define F_CPU 2000000UL //change this
-#define _latPort PORTC
-#define _latPin 0
-#define _SSPort PORTC
-#define _SSPin 4
+#define F_CPU 2000000UL
 
 #include <avr/io.h>
 #include <avr/xmega.h>
@@ -19,57 +15,41 @@
 #include <string.h>
 #include <util/delay.h>
 
-#define NUM_BYTES     4
-
+#include "SPI.h"
 
 void clock_init(void);
+
 
 int main(void)
 {
 	clock_init();
-	
-	_latPort.DIRSET |= 1 << _latPin; //Latch pin for TLC5947
-	_latPort.DIRSET |= 1 << 1; //BLANK pin for TLC5947
-	_latPort.DIRSET |= 1 << 7; //SCK pin for TLC5947
-	_latPort.DIRSET |= 1 << 5; //MOSI pin for TLC5947
-
+	spi_init();
 
 	PORTA.DIRSET = 0b11111111; //ROW2
-
-	_SSPort.PIN4CTRL |= PORT_OPC_PULLUP_gc;
-	_SSPort.DIRSET |= 1 << _SSPin; //Slave select
-	_SSPort.OUTSET |= 1 << _SSPin; //Slave select HIGH
 	PORTA.OUTSET = 0b11111111; //ROW2
 
-	SPIC.CTRL |= SPI_ENABLE_bm | SPI_MASTER_bm | SPI_MODE_0_gc;
-	SPIC.INTCTRL |= SPI_INTLVL_OFF_gc;
 
-		_latPort.OUTCLR |= 1 << _latPin;
-
-		SPIC.DATA = 0b11111111;
+	
+	for(int i = 0; i < 36; i++){
+		SPIC.DATA = 0b00101100;
 		while(!(SPIC.STATUS & SPI_IF_bm)){}
-		SPIC.DATA = 0b11110000;
-		while(!(SPIC.STATUS & SPI_IF_bm)){}
-
-		for(int i = 0; i < 34; i++){
-			SPIC.DATA = 0b00000000;
-			while(!(SPIC.STATUS & SPI_IF_bm)){}
-		}
-		_latPort.OUTSET |= 1 << _latPin;
-		_latPort.OUTCLR |= 1 << _latPin;
+		_delay_ms(1);
+	}
+	
+	SPI_blankAndLatch();
 
 
 	
 	while (1) 
     {
-	if(PORTA.OUT == 0){
-		PORTA.OUT+=1;
-		_delay_ms(1);
-	}
+		if(PORTA.OUT == 0){
+			PORTA.OUT+=1;
+			_delay_ms(1);
+		}
 		PORTA.OUT = PORTA.OUT << 1;
-		
 		_delay_ms(1);
     }
+	
 }
 
 void clock_init(){
@@ -77,3 +57,5 @@ void clock_init(){
 	while(!(OSC.STATUS & OSC_RC2MRDY_bm));
 	_PROTECTED_WRITE(CLK_CTRL, CLK_SCLKSEL_RC2M_gc);
 }
+
+
